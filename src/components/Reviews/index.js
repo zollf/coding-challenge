69f5c0001a85c18e51f42657aee04b0532319c5c
Dylan Kennedy <Dylan@Dylans-MBP.lan>
2020-08-06 22:10:47 +0800

    Review structure update

--- src/components/Reviews/index.js
+++ src/components/Reviews/index.js
@@ -1,21 +1,32 @@
 import React from 'react';
 
+import './Reviews.css';
+
 import starIcon from 'images/star.svg';
 
 export default function Reviews({ reviews }) {
   return (
-    <div>
-      <h2>All Reviews</h2>
+    <div class = "container-fluid">
+      <div class = "row">
+        <div class = "col-md-6 col-7 col-centered">
+          <h2>All Reviews</h2>
+          <div class = "card-group justify-content-center">
 
-      {reviews.map(review => (
-        <div key={review.name} data-testid="review">
-          <p>Name?</p>
-          <div>
-            <img src={starIcon} alt="" />
+            
+
+            {reviews.map(review => (
+              <div class = "review-block card-body" key={review.name} data-testid="review">
+                <p>{review.name}</p>
+                <div>
+                  <img src={starIcon} alt="" />
+                </div>
+                <p>{review.comment}</p>
+              </div>
+            ))}
+
           </div>
-          <p>Description?</p>
-        </div>
-      ))}
+        </div>  
+      </div>
     </div>
   )
 }
